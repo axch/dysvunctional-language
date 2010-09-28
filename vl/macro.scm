112197c69955e00fdbab167b30e84cb3a5cc51a6
Alexey Radul <axch@mit.edu>
2010-09-28 16:15:01 +0100

    Explaining the macroexpander.

--- vl/macro.scm
+++ vl/macro.scm
@@ -1,3 +1,14 @@
+;;;; Syntactic preprocessor
+
+;;; Accepts VL surface syntax and converts it into the more restricted
+;;; language that the VL interpreter and the VL compiler actually
+;;; process.  This is basically a rather straightforward, unhygienic
+;;; macro engine; but since VL does not allow user-defined macros, the
+;;; lack of hygiene is not too bad.
+
+;;; Macro expansion proceeds by structural recursion over the form
+;;; being expanded.
+
 (define (macroexpand exp)
   (cond ((variable? exp)
 	 exp)
@@ -18,6 +29,9 @@
 	(else
 	 (error "Invalid expression syntax" exp))))
 
+;;; A VL procedure application supplying zero or 2+ arguments needs to
+;;; be converted into code that CONSes up the right structure and
+;;; passes it in.
 (define (macroexpand-operands operands)
   (cond ((null? operands)
 	 '())
@@ -27,6 +41,10 @@
 	 `(cons ,(macroexpand (car operands))
 		,(macroexpand-operands (cdr operands))))))
 
+;;; A VL procedure definition that specifies zero or 2+ formal
+;;; parameters needs to be converted into one that specifies the right
+;;; destructuring.  While I'm at it, I can allow explicit
+;;; destructuring in the surface syntax.
 (define (macroexpand-formals formals)
   (cond ((null? formals)
 	 (list formals))
@@ -47,6 +65,8 @@
 		   (error "Invalid formal parameter tree" formals))))))
 	(else
 	 (error "Invalid formal parameter tree" formals))))
+
+;;;; Additional data-driven macros.
 
 (define *vl-macros* '())
 
@@ -62,6 +82,7 @@
 (define (define-vl-macro! name transformer)
   (set! *vl-macros* (cons (cons name transformer) *vl-macros*)))
 
+;;; LET
 (define (normal-let-transformer form)
   (let ((bindings (cadr form))
 	(body (cddr form)))
@@ -69,6 +90,7 @@
 	,@body)
       ,@(map cadr bindings))))
 
+;;; Named LET
 (define (named-let-transformer form)
   (let ((name (cadr form))
 	(bindings (caddr form))
@@ -83,12 +105,28 @@
 	(named-let-transformer form)
 	(normal-let-transformer form))))
 
+;;; Following the suggestion in [1], I chose to do IF as a macro
+;;; expansion into a primitive IF-PROCEDURE.  This may have been a bad
+;;; idea, because the analyzer and code generator seem to need to
+;;; special-case applications of this primitive procedure anyway, so
+;;; why not leave IF as a special form in the language?
 (define-vl-macro! 'if
   (lambda (form)
     `(if-procedure
       ,(cadr form)
       (lambda () ,(caddr form))
       (lambda () ,(cadddr form)))))
+
+;;;; LETREC
+
+;;; LETREC by transformation to the Z combinator.  The Z combinator is
+;;; a slightly complexified Y combinator that works in a strict
+;;; language.  Also multi-binding LETREC by transformation to an n-ary
+;;; version of the Z combinator (this one with an explicit
+;;; continuation so it can produce multiple things sensibly).  Do not
+;;; read this macro definition unless you are a lambda calculus geek,
+;;; and even then I advise looking at some examples of its output
+;;; first.
 
 (define (letrec-transformer form)
   (let ((bindings (cadr form))
