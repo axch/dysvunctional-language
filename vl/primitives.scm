51a886e4638b6daabf066d26defda955d66a3778
Alexey Radul <axch@mit.edu>
2010-09-28 16:45:49 +0100

    Explaining the primitives.

--- vl/primitives.scm
+++ vl/primitives.scm
@@ -1,22 +1,28 @@
+;;;; VL primitive procedures
+
+;;; A VL primitive procedure needs to tell the concrete evaluator how
+;;; to execute it, the analyzer how to think about calls to it, and
+;;; the code generator how to emit calls to it.  These are the
+;;; implementation, abstract-implementation, and name and arity slots,
+;;; respectively.
+
 (define-structure (primitive (safe-accessors #t))
   name
   arity
   implementation
   abstract-implementation)
 
-(define (uncurry f)
-  (lambda (lst)
-    (f (car lst) (cadr lst))))
-
 (define *primitives* '())
 
 (define (add-primitive! primitive)
   (set! *primitives* (cons primitive *primitives*)))
 
+;;; Most primitives fall into a few natural classes:
+
+;;; Unary numeric primitives just have to handle getting abstract
+;;; values for arguments (either ABSTRACT-ALL or ABSTRACT-REAL).
 (define (unary-numeric-primitive name base)
-  (make-primitive
-   name
-   1
+  (make-primitive name 1
    base
    (lambda (arg)
      (if (abstract-all? arg)
@@ -25,10 +31,10 @@
 	     abstract-real
 	     (base arg))))))
 
+;;; Binary numeric primitives also have to destructure their input,
+;;; because the VL system will hand it in as a pair.
 (define (binary-numeric-primitive name base)
-  (make-primitive
-   name
-   2
+  (make-primitive name 2
    (lambda (arg)
      (base (car arg) (cdr arg)))
    (lambda (arg)
@@ -40,11 +46,29 @@
 		   (abstract-real? second-arg))
 	       abstract-real
 	       (base first-arg second-arg)))))))
+
+;;; Primitive type testers (namely NULL? and PAIR?) need to take care
+;;; not to offer information too soon.  In principle, of course, no
+;;; matter what argument is passed to NULL? or PAIR?, the answer is
+;;; guaranteed to be some boolean.  However, IF treats an
+;;; ABSTRACT-BOOLEAN predicate as a promise that the analysis has
+;;; figured out that either branch is definitely capable of being
+;;; taken.  Therefore, primitive type testers must wait until the
+;;; analysis has determined the shape their argument will take, and
+;;; compute the answer only then.
 
+(define (primitive-type-predicate name base)
+  (make-primitive name 1
+   base
+   (lambda (arg)
+     (if (abstract-all? arg)
+	 abstract-all ; Not abstract-bool, we're union-free
+	 (base arg)))))
+
+;;; Binary numeric comparisons have all the concerns of binary numeric
+;;; procedures and of unary type testers.
 (define (RxR->bool-primitive name base)
-  (make-primitive
-   name
-   2
+  (make-primitive name 2
    (lambda (arg)
      (base (car arg) (cdr arg)))
    (lambda (arg)
@@ -56,25 +80,7 @@
 		   (abstract-real? second))
 	       abstract-boolean
 	       (base first second)))))))
-
-(define (primitive-type-predicate name base)
-  (make-primitive
-   name
-   1
-   base
-   (lambda (arg)
-     (if (abstract-all? arg)
-	 abstract-all ; Not abstract-bool, we're union-free
-	 (base arg)))))
-
-(define (real x)
-  (if (real? x)
-      x
-      (error "A non-real object is asserted to be real" x)))
-
-(define (if-procedure p c a)
-  (if p (c) (a)))
-
+
 (define-syntax define-unary-numeric-primitive
   (syntax-rules ()
     ((_ name)
@@ -118,14 +124,24 @@
 (define-RxR->bool-primitive >=)
 (define-RxR->bool-primitive  =)
 
-;; TODO Do these really have to be primitive?
 (define-primitive-type-predicate null?)
 (define-primitive-type-predicate pair?)
+
+;;; The primitives REAL and IF-PROCEDURE are very special.
+
+(define (real x)
+  (if (real? x)
+      x
+      (error "A non-real object is asserted to be real" x)))
+
+;;; REAL must take care to always emit an ABSTRACT-REAL during
+;;; analysis, even though it's the identity function at runtime.
+;;; Without this, "union-free flow analysis" would amount to running
+;;; the program very slowly at analysis time until the final answer
+;;; was computed.
 
 (add-primitive!
- (make-primitive
-  'real
-  1
+ (make-primitive 'real 1
   real
   (lambda (x)
     (cond ((abstract-all? x) abstract-all)
@@ -133,10 +149,16 @@
 	  ((number? x) abstract-real)
 	  (else (error "Something known not to be a real number is declared real" x))))))
 
+(define (if-procedure p c a)
+  (if p (c) (a)))
+
+;;; IF is even more special than REAL.  This definition does not
+;;; capture how special IF really is, because the analyzer and the
+;;; code generator check explicitly for whether they've hit an IF or
+;;; not.  I am open to suggestions about how to improve this.
+
 (define primitive-if
-  (make-primitive
-   'if-procedure
-   3
+  (make-primitive 'if-procedure 3
    (lambda (arg)
      (if-procedure (car arg) (cadr arg) (cddr arg)))
    (lambda (shape analysis)
@@ -152,5 +174,5 @@
 (add-primitive! primitive-if)
 
 (define (abstract-result-of thunk-shape analysis)
-  ;; This operation only exists because of the way I'm doing IF.
+  ;; N.B. ABSTRACT-RESULT-OF only exists because of the way I'm doing IF.
   (refine-apply thunk-shape '() analysis))
