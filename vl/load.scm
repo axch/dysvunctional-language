2e909945ec3871609b669f0251d57f9230983055
Alexey Radul <axch@mit.edu>
2010-10-02 05:29:46 -0400

    Installing my automatic compilation support file so as to be able to run the code compiled.

--- vl/load.scm
+++ vl/load.scm
@@ -2,16 +2,6 @@
 
 ;;; Why are you reading this file?  You already know what it does.
 
-(define (cf-conditionally filename)
-  (fluid-let ((sf/default-syntax-table (nearest-repl/environment)))
-    (sf-conditionally filename))
-  (if (not (file-processed? filename "bin" "com"))
-      (compile-bin-file filename)))
-
-(define (load-compiled filename)
-  (cf-conditionally filename)
-  (load filename))
-
 (define (self-relatively thunk)
   (if (current-eval-unit #f)
       (with-working-directory-pathname
@@ -22,8 +12,7 @@
 (define (load-relative filename)
   (self-relatively (lambda () (load filename))))
 
-(define (load-relative-compiled filename)
-  (self-relatively (lambda () (load-compiled filename))))
+(load-relative "support/auto-compilation")
 
 (for-each
  load-relative
