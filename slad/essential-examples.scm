f7a0a0f4adf9d21e513eb2e71b35aff92076d66d
Alexey Radul <axch@mit.edu>
2011-01-28 19:59:00 +0000

    A listing of examples that constitute essential tests.  Part of the purpose is to be minimal, both with respect to implementation effort and with respect to test count, so you can quickly tell whether your system is working.

---
+++ slad/essential-examples.scm
@@ -0,0 +1,33 @@
+;;; Identity function
+((derivative (lambda (x) x)) 7) ===> 1
+
+;;; Constant function
+((derivative (lambda (x) 3)) 7) ===> 0
+
+;;; Transform of +
+((derivative (lambda (x) (+ x 1))) 7) ===> 1
+((derivative (lambda (x) (+ x x))) 7) ===> 2
+
+;;; Nested constant function (don't conflate the perturbations)
+(let ()
+  (define (one x)
+    ((derivative (lambda (y) (+ x y))) 3))
+  ((derivative one) 7)) ===> 0
+
+;;; Another don't conflate the perturbations
+(let ()
+  (define (one x)
+    ((derivative (lambda (y) (+ x y))) 3))
+  ((derivative (lambda (x)
+		 (* x (one x))))
+   7)) ===> 1
+
+;;; Don't confuse the perturbations
+;; I'm not sure this would catch a thing that swapped the
+;; perturbations with each other, but it at least might.
+(let ()
+  (define (one x)
+    ((derivative (lambda (y) (+ x y))) 3))
+  ((derivative (lambda (x)
+		 (* x (one (* 2 x)))))
+   7)) ===> 1
