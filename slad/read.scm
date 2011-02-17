cba9b1032229f23c93e153cc38cea8f52789cd5e
Alexey Radul <axch@mit.edu>
2011-02-17 13:50:18 +0000

    Explaining the reader.

--- slad/read.scm
+++ slad/read.scm
@@ -1,4 +1,14 @@
 (declare (usual-integrations))
+;;;; Reader
+
+;;; This is a little wrapper around the underlying MIT Scheme reader
+;;; that does two interesting things: it parses (include "foo")
+;;; directives (at the top level only) by recursively reading those
+;;; files and splicing their contents into the current one (beware, no
+;;; loop detection); and it interprets the symbol ===> as an
+;;; s-expression comment when used at the top level.  The latter is
+;;; for compatibility with a testing framework that would use that
+;;; symbol to delimit expected answers.
 
 (define (default-extension-to-vlad pathname)
   (merge-pathnames (->pathname pathname) (->pathname "foo.vlad")))
