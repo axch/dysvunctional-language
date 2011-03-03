a5416863253c6f3e21dee10a7aa7b175cac6a7bd
Alexey Radul <axch@mit.edu>
2011-03-03 14:53:26 +0000

    An attempt at abstract syntax for the output language.

--- vl/output-syntax.scm
+++ vl/output-syntax.scm
@@ -1,6 +1,8 @@
 (declare (usual-integrations))
 ;;;; Syntax and manipulations of the output language
 
+(define let-form? (tagged-list? 'let))
+
 (define (constructors-only? exp)
   (or (symbol? exp)
       (constant? exp)
@@ -21,52 +23,50 @@
 
 (define (count-free-occurrences name exp)
   (cond ((eq? exp name) 1)
+	((lambda-form? exp)
+	 (if (occurs-in-tree? name (lambda-formal exp))
+	     0
+	     (count-free-occurrences name (cddr exp))))
+	((let-form? exp)
+	 (let ((bindings (cadr exp))
+	       (body (cddr exp)))
+	   (apply +
+		  (if (memq name (map car bindings))
+		      0
+		      (count-free-occurrences name body))
+		  (map (lambda (binding)
+			 (count-free-occurrences name (cadr binding)))
+		       bindings))))
+	((pair-form? exp)
+	 (+ (count-free-occurrences name (car-subform exp))
+	    (count-free-occurrences name (cdr-subform exp))))
 	((pair? exp)
-	 (cond ((eq? (car exp) 'lambda)
-		(if (occurs-in-tree? name (cadr exp))
-		    0
-		    (count-free-occurrences name (cddr exp))))
-	       ((eq? (car exp) 'let)
-		(let ((bindings (cadr exp))
-		      (body (cddr exp)))
-		  (apply +
-		   (if (memq name (map car bindings))
-		       0
-		       (count-free-occurrences name body))
-		   (map (lambda (binding)
-			  (count-free-occurrences name (cadr binding)))
-			bindings))))
-	       ((eq? (car exp) 'cons)
-		(+ (count-free-occurrences name (cadr exp))
-		   (count-free-occurrences name (caddr exp))))
-	       (else
-		(+ (count-free-occurrences name (car exp))
-		   (count-free-occurrences name (cdr exp))))))
+	 (+ (count-free-occurrences name (car exp))
+	    (count-free-occurrences name (cdr exp))))
 	(else 0)))
 
 (define (replace-free-occurrences name new exp)
   (cond ((eq? exp name) new)
+	((lambda-form? exp)
+	 (if (occurs-in-tree? name (lambda-formal exp))
+	     exp
+	     `(lambda ,(lambda-formal exp)
+		,@(replace-free-occurrences name new (cddr exp)))))
+	((let-form? exp)
+	 (let ((bindings (cadr exp))
+	       (body (cddr exp)))
+	   `(let ,(map (lambda (binding)
+			 (list (car binding)
+			       (replace-free-occurrences
+				name new (cadr binding))))
+		       bindings)
+	      ,@(if (memq name (map car bindings))
+		    body
+		    (replace-free-occurrences name new body)))))
+	((pair-form? exp)
+	 `(cons ,(replace-free-occurrences name new (car-subform exp))
+		,(replace-free-occurrences name new (cdr-subform exp))))
 	((pair? exp)
-	 (cond ((eq? (car exp) 'lambda)
-		(if (occurs-in-tree? name (cadr exp))
-		    exp
-		    `(lambda ,(cadr exp)
-		       ,@(replace-free-occurrences name new (cddr exp)))))
-	       ((eq? (car exp) 'let)
-		(let ((bindings (cadr exp))
-		      (body (cddr exp)))
-		  `(let ,(map (lambda (binding)
-				(list (car binding)
-				      (replace-free-occurrences
-				       name new (cadr binding))))
-			      bindings)
-		     ,@(if (memq name (map car bindings))
-			   body
-			   (replace-free-occurrences name new body)))))
-	       ((eq? (car exp) 'cons)
-		`(cons ,(replace-free-occurrences name new (cadr exp))
-		       ,(replace-free-occurrences name new (caddr exp))))
-	       (else
-		(cons (replace-free-occurrences name new (car exp))
-		      (replace-free-occurrences name new (cdr exp))))))
+	 (cons (replace-free-occurrences name new (car exp))
+	       (replace-free-occurrences name new (cdr exp))))
 	(else exp)))
