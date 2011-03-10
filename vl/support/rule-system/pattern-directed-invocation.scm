a01fc5cc066f1a3424a35b28080661cfe432bbbc
Alexey Radul <axch@mit.edu>
2011-03-10 17:42:08 +0000

    When in doubt, dike it out.  I have no use for pattern-operators in this use case of this rule system.

--- vl/support/rule-system/pattern-directed-invocation.scm
+++ vl/support/rule-system/pattern-directed-invocation.scm
@@ -24,19 +24,6 @@
 	     (handler dict succeed fail))
 	   fail)))))
 
-;;; A pattern directed operator is a collection of rules, one of which
-;;; is expected to match any datum that the operator may be given.
-;;; The operator tries the rules in order until the first matches, and
-;;; returns the value given by that one; if none match, it errors out.
-
-(define (make-pattern-operator #!optional rules)
-  (define (operator self . arguments)
-    (define (succeed value fail) value)
-    (define (fail)
-      (error "No applicable operations" self arguments))
-    (try-rules arguments (entity-extra self) succeed fail))
-  (make-entity operator (if (default-object? rules) '() rules)))
-
 (define (try-rules data rules succeed fail)
   (let per-rule ((rules rules))
     (if (null? rules)
@@ -44,10 +31,6 @@
 	((car rules) data succeed
 	 (lambda ()
 	   (per-rule (cdr rules)))))))
-
-(define (attach-rule! operator rule)
-  (set-entity-extra! operator
-   (cons rule (entity-extra operator))))
 
 ;;; The user-handler is expected to be a procedure that binds the
 ;;; variables that appear in the match and uses them somehow.  This
@@ -108,46 +91,3 @@
   ;; See magic in utils.scm
   (make-lambda names env
     (lambda (env*) (close-syntax form env*))))
-
-#|
- (pp (syntax '(rule '(* (? a) (? b))
-		    (and (expr<? a b)
-                         `(* ,a ,b)))
-             (the-environment)))
-
-; (make-rule '(* (? a) (? b))
-;  (lambda (b a)
-;    (and (expr<? a b)
-;         (list '* a b))))
-;Unspecified return value
-|#
-
-;;; Pattern-directed factorial, with and without the rule macro.
-
-#|
- (define factorial (make-pattern-operator))
-
- (attach-rule! factorial (rule '(0) 1))
-
- (attach-rule! factorial
-  (rule `((? n ,positive?))
-        (* n (factorial (- n 1)))))
-
- (factorial 10)
- ;Value: 3628800
-|#
-
-#|
- (define factorial (make-pattern-operator))
-
- (attach-rule! factorial
-  (make-rule '((? n))
-   (lambda (n) (* n (factorial (- n 1))))))
-
- (attach-rule! factorial
-  (make-rule '(0) (lambda () 1)))
-
- (factorial 10)
- ;Value: 3628800
-|#
-
