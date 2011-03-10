86ecb8aa5e1e4e75c9a18a613610f5c51329cac3
Alexey Radul <axch@mit.edu>
2011-03-10 16:28:37 +0000

    I don't actually need these rule systems.

--- vl/support/rule-system/rules.scm
+++ vl/support/rule-system/rules.scm
@@ -1,22 +1,4 @@
-;;;; File:  rules.scm -- Some sample algebraic simplification rules
-
 (declare (usual-integrations))
-
-(define algebra-1
-  (rule-simplifier
-   (list
-    ;; Associative law of addition
-    (rule '(+ (? a) (+ (? b) (? c)))
-	  `(+ (+ ,a ,b) ,c))
-
-    ;; Commutative law of multiplication
-    (rule '(* (? b) (? a))
-	  (and (expr<? a b)
-	       `(* ,a ,b)))
-
-    ;; Distributive law of multiplication over addition
-    (rule '(* (? a) (+ (? b) (? c)))
-	  `(+ (* ,a ,b) (* ,a ,c))) )))
 
 (define (list<? x y)
   (let ((nx (length x)) (ny (length y)))
@@ -47,72 +29,3 @@
      (,number? . ,<)
      (,symbol? . ,symbol<?)
      (,list?   . ,list<?))))
-#|
- (algebra-1 '(* (+ y (+ z w)) x))
- ;Value: (+ (+ (* x y) (* x z)) (* w x))
-|#
-
-(define algebra-2
-  (rule-simplifier
-   (list
-
-    ;; Sums
-
-    (rule `(+ (? a)) a)
-
-    (rule `(+ (?? a) (+ (?? b)))
-	  `(+ ,@a ,@b))
-
-    (rule `(+ (+ (?? a)) (?? b))
-	  `(+ ,@a ,@b))
-
-    (rule `(+ (?? a) (? y) (? x) (?? b))
-	  (and (expr<? x y)
-	       `(+ ,@a ,x ,y ,@b)))
-    
-
-    ;; Products
-
-    (rule `(* (? a)) a)
-
-    (rule `(* (?? a) (* (?? b)))
-	  `(* ,@a ,@b))
-
-    (rule `(* (* (?? a)) (?? b))
-	  `(* ,@a ,@b))
-
-    (rule `(* (?? a) (? y) (? x) (?? b))
-	  (and (expr<? x y)
-	       `(* ,@a ,x ,y ,@b)))
-
-
-    ;; Distributive law
-
-    (rule `(* (? a) (+ (?? b)))
-	  `(+ ,@(map (lambda (x) `(* ,a ,x)) b)))
-
-
-    ;; Numerical simplifications below
-
-    (rule `(+ 0 (?? x)) `(+ ,@x))
-
-    (rule `(+ (? x ,number?) (? y ,number?) (?? z))
-	  `(+ ,(+ x y) ,@z))
-
-
-    (rule `(* 0 (?? x)) 0)
-     
-    (rule `(* 1 (?? x)) `(* ,@x))
-
-    (rule `(* (? x ,number?) (? y ,number?) (?? z))
-	  `(* ,(* x y) ,@z))
-
-    )))
-
-#|
- (algebra-2 '(* (+ y (+ z w)) x))
- ;Value: (+ (* w x) (* x y) (* x z))
-
- (algebra-2 '(+ (* 3 (+ x 1)) -3))
- ;Value: (* 3 x)
-|#
