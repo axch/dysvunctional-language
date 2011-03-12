c5b03ed49fee3c5827cd9fc26c71e5f605203df9
Alexey Radul <axch@mit.edu>
2011-03-12 14:16:56 +0000

    These testing utilities can now be shared.

--- dvl/test/load.scm
+++ dvl/test/load.scm
@@ -1,48 +1,5 @@
 (load-relative "../../testing/load")
-
-(define (analyzed-answer program)
-  (let ((candidate
-	 (let ((full-prog (macroexpand program)))
-	   (let loop ((bindings (analysis-bindings (analyze program))))
-	     (cond ((null? bindings) #f)
-		   ((equal? full-prog (binding-exp (car bindings)))
-		    (car bindings))
-		   (else (loop (cdr bindings))))))))
-    (if (not candidate)
-	(error "Analysis makes no binding for the original program"
-	       program)
-	(binding-value candidate))))
-
-(define (determined-form-breakage value form)
-  (cond ((not (equal? (macroexpand form) (macroexpand (macroexpand form))))
-	 `(not (equal? ,(macroexpand form) ,(macroexpand (macroexpand form)))))
-	((not (equal? value (interpret form)))
-	 `(not (equal? ,value (interpreted ,(interpret form)))))
-	((not (equal? value (analyzed-answer form)))
-	 `(not (equal? ,value (analyzed ,(analyzed-answer form)))))
-	((not (equal? `(begin ,value) (analyze-and-generate form)))
-	 `(not (equal? ,value (compiled ,(analyze-and-generate form)))))
-	(else #f)))
-
-(define (%scheme-eval code)
-  (eval code (nearest-repl/environment)))
-
-(define (eval-through-scheme program)
-  (let* ((interpreted-answer (interpret program))
-	 (analysis (analyze program))
-	 (compiled-program (generate program analysis))
-	 (compiled-answer (%scheme-eval compiled-program))
-	 (pretty-compiled-answer (%scheme-eval (prettify-compiler-output compiled-program)))
-	 (direct-pretty-compiled-answer (%scheme-eval (compile-to-scheme program))))
-    (if (and (equal? interpreted-answer compiled-answer)
-	     (equal? interpreted-answer pretty-compiled-answer)
-	     (equal? interpreted-answer direct-pretty-compiled-answer))
-	compiled-answer
-	(error "Compiler disagreed with interpreter"
-	       `((interpreted: ,interpreted-answer)
-		 (compiled: ,compiled-answer)
-		 (compiled-and-prettified ,pretty-compiled-answer)
-		 (pretty-compiled ,direct-pretty-compiled-answer))))))
+(load-relative "../../vl/test/utils")
 
 (in-test-group
  dvl
