a3ee936dda98aff5be5faf403297555c009ab77b
Alexey Radul <axch@mit.edu>
2011-03-12 15:14:24 +0000

    Drafting a more uniform pile of utilities for carrying out compilation while checking invariants.

--- vl/test/utils.scm
+++ vl/test/utils.scm
@@ -1,6 +1,7 @@
 ;;; Invariants:
 ;;;   macroexpand is idempotent
-;;;   interpret produces the same answer as analyze, generate, and eval
+;;;   macroexpand produces syntactically correct kernel language
+;;;   interpret produces the same answer as analyze-generate-eval
 ;;;   for solved programs, analyze binds the answer to the program
 ;;;     and generate emits `(begin ,answer).
 ;;;   each stage of FOL compilation does not affect the answer
@@ -9,6 +10,62 @@
 ;;;   for union-free programs, optimized FOL does not cons
 ;;;   the code generator emits syntactically correct and type correct FOL
 ;;;   FOL compilation preserves syntactic and type correctness stepwise
+
+(define (careful-macroexpand program)
+  (let ((answer (macroexpand program)))
+    (check (equal? answer (macroexpand answer)))
+    answer))
+
+(define (careful-solved-analyze kernel-program answer)
+  (let ((analysis (analyze kernel-program)))
+    (let loop ((bindings (analysis-bindings analysis)))
+      (cond ((null? bindings)
+             (error "Analysis makes no binding for the original program"
+                    program))
+            ((equal? kernel-program (binding-exp (car bindings)))
+             (check (equal? answer (binding-value (car bindings))))
+             analysis)
+            (else (loop (cdr bindings)))))))
+
+(define (careful-generate program analysis answer)
+  (let ((raw-fol (generate program analysis #t)))
+    (check (equal? answer (fol-eval raw-fol)))
+    raw-fol))
+
+(define (careful-solved-generate program analysis answer)
+  (let ((raw-fol (strip-argument-types
+                  (careful-generate program analysis answer))))
+    (check (equal? `(begin ,answer) raw-fol))
+    raw-fol))
+
+(define ((fol-carefully stage) fol-code answer)
+  (let ((done (stage fol-code)))
+    (check (equal? answer (fol-eval done)))
+    (check (equal? done (stage done)))
+    done))
+
+(define (determined-answer program)
+  (let* ((kernel (careful-macroexpand program))
+         (answer (interpret kernel))
+         (analysis (careful-solved-analyze kernel answer))
+         (raw-fol (careful-solved-generate kernel analysis answer)))
+    answer))
+
+(define (union-free-answer program)
+  (let* ((kernel (careful-macroexpand program))
+         (answer (interpret kernel))
+         (analysis (analyze kernel))
+         (raw-fol (careful-generate kernel analysis answer))
+         (vectors-fol ((fol-carefully structure-definitions->vectors)
+                       raw-fol answer))
+         (inlined ((fol-carefully inline) vectors-fol answer))
+         (scalars ((fol-carefully scalar-replace-aggregates) inlined answer))
+         (alpha ((fol-carefully full-alpha-rename) scalars answer))
+         (tidied ((fol-carefully tidy) alpha answer)))
+    (check (= 0 (count-free-occurrences 'car tidied)))
+    (check (= 0 (count-free-occurrences 'cdr tidied)))
+    (check (= 0 (count-free-occurrences 'vector-ref tidied)))
+    answer))
 
 (define (analyzed-answer program)
   (let ((full-prog (macroexpand program)))
