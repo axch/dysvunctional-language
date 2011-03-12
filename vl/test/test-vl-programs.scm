0e519585e9bedb3a3f3f441e8f358ed7cff14b15
Alexey Radul <axch@mit.edu>
2011-03-12 16:01:53 +0000

    The test VL programs have answers now too.

--- vl/test/test-vl-programs.scm
+++ vl/test/test-vl-programs.scm
@@ -2,31 +2,31 @@
 
 (if (< (real 3) (real 6))
     (real 4)
-    (real 3))
+    (real 3)) ===> 4
 
 (if (< 3 6)
     (real 4)
-    (real 3))
+    (real 3)) ===> 4
 
 ;;; Variations on destructuring
 
 (let ((my-add (lambda (x y) (+ (real x) (real y)))))
-  (my-add 3 6))
+  (my-add 3 6)) ===> 9
 
 (let ((my-add (lambda (foo) (+ foo))))
-  (my-add (cons (real 3) (real 6))))
+  (my-add (cons (real 3) (real 6)))) ===> 9
 
 (let ((my-add (lambda (foo) (+ foo))))
-  (my-add (cons 3 (real 6))))
+  (my-add (cons 3 (real 6)))) ===> 9
 
 (let ((my-add (lambda (foo) (real (+ foo)))))
-  (my-add 3 6))
+  (my-add 3 6)) ===> 9
 
 (let ((delay-add (lambda (x y) (lambda () (+ x y)))))
-  ((delay-add (real 3) (real 6))))
+  ((delay-add (real 3) (real 6)))) ===> 9
 
 (let ((frobnicate (lambda (x) (real x))))
-  (frobnicate 3))
+  (frobnicate 3)) ===> 3
 
 ;;; Recursion with non-primitive procedures
 
@@ -35,14 +35,14 @@
 		   (if (= n 1)
 		       1
 		       (my-* n (fact (- n 1)))))))
-    (fact (real 5))))
+    (fact (real 5)))) ===> 120
 
 (let ((my-* (lambda (x y) (* x y))))
   (letrec ((fact (lambda (n)
 		   (if (= n 1)
 		       1
 		       (my-* n (fact (- n 1)))))))
-    (fact 5)))
+    (fact 5))) ===> 120
 
 ;;; Factorial, with letrec manually macro expanded
 
@@ -56,5 +56,5 @@
 		     (if (= n 1)
 			 1
 			 (* n (fact (- n 1)))))))))
-    (fact (real 5))))
+    (fact (real 5)))) ===> 120
 
