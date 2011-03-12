15cc863b9b6f20c880a27a0ec5c8ca77f24dc754
Alexey Radul <axch@mit.edu>
2011-03-12 23:43:15 +0000

    Sanity check.

--- vl/test/post-processing-test.scm
+++ vl/test/post-processing-test.scm
@@ -2,6 +2,7 @@
  post-processing
 
  (define-each-check
+   (lset= eq? '(a d) (feedback-vertex-set '((a b c d) (b a) (c d) (d c) (e a))))
    (not (tidy-non-soundness
 	 ;; Carelessly inlining y will change the scope of x
 	 '(let ((x (vector 1 2)))
