5f0c13080ffeb77356ff4e60835de315bf5e6c91
Alexey Radul <axch@mit.edu>
2011-03-14 14:10:07 +0000

    A test of compiler output being reasonable.

--- vl/test/post-processing-test.scm
+++ vl/test/post-processing-test.scm
@@ -8,4 +8,24 @@
 	 '(let ((x (vector 1 2)))
 	    (cons x (cons x (let ((y (vector 3 x))
 				  (x (vector 4)))
-			      (cons x (cons x (vector-ref (vector-ref y 1) 1)))))))))))
+			      (cons x (cons x (vector-ref (vector-ref y 1) 1)))))))))
+
+   ;; TODO How can I get the inlining not to clone the termination condition like that?
+   (alpha-rename?
+    '(begin
+       (define (operation-4 the-closure-12)
+         (* the-closure-12
+            (let ((the-formals-26 (- the-closure-12 1)))
+              (if (= the-formals-26 1)
+                  1
+                  (operation-4 the-formals-26)))))
+       (if (= (real 5) 1)
+           1
+           (operation-4 (real 5))))
+    (compile-to-scheme
+     '(let ()
+        (define (fact n)
+          (if (= n 1)
+              1
+              (* n (fact (- n 1)))))
+        (fact (real 5)))))))
