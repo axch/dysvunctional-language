c8f56e1488428aac2d9e35a453083199bff4fdcb
Alexey Radul <axch@mit.edu>
2011-03-18 21:29:09 +0000

    Some more tests that the compiler produces good output on small examples.

--- vl/test/post-processing-test.scm
+++ vl/test/post-processing-test.scm
@@ -26,6 +26,61 @@
         (fact (real 5)))))
 
    (alpha-rename?
+    '(cons
+      (let ((x (* (real 2) (real 2))))
+        (+ x x))
+      (let ((x (+ (real 2) (real 2))))
+        (* x x)))
+    (compile-to-scheme
+     '(let ((double (lambda (x) (+ x x)))
+            (square (lambda (x) (* x x)))
+            (compose (lambda (f g) (lambda (x) (f (g x))))))
+        (cons ((compose double square) (real 2))
+              ((compose square double) (real 2))))))
+
+   ;; Note the extra variable the-closure-53, below.  This is a use
+   ;; case for interprocedural alias elimination.
+   (alpha-rename?
+    '(begin
+       (define (operation-9 the-closure-41
+                            the-closure-53
+                            the-formals-56
+                            the-formals-57)
+         (argument-types (the-closure-41 real)
+                         (the-closure-53 real)
+                         (the-formals-56 real)
+                         (the-formals-57 real)
+                         real)
+         (if (< (abs (- the-formals-56 the-formals-57)) .00001)
+             the-formals-57
+             (operation-9
+              the-closure-53
+              the-closure-53
+              the-formals-57
+              (/ (+ the-formals-57 (/ the-closure-41 the-formals-57)) 2))))
+       (cons
+        1.4142135623730951
+        (operation-9 (real 2)
+                     (real 2)
+                     (real 1.)
+                     (/ (+ (real 1.) (/ (real 2) (real 1.))) 2))))
+    (compile-to-scheme
+     '(let ()
+        (define (heron-step x)
+          (lambda (guess)
+            (/ (+ guess (/ x guess)) 2)))
+        (define (close-enuf? a b)
+          (< (abs (- a b)) .00001))
+        (define (numeric-fix f start close-enuf?)
+          (let loop ((old start) (new (f start)))
+            (if (close-enuf? old new)
+                new
+                (loop new (f new)))))
+        (define (square-root x)
+          (numeric-fix (heron-step x) (real 1.) close-enuf?))
+        (cons (sqrt 2) (square-root (real 2))))))
+
+   (alpha-rename?
     '(if (< (real 1) (real 2))
          (real 0)
          (+ (real 1) (real 2)))
