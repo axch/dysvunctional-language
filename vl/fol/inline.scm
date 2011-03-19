c2183e9af8189abff5fdbe8e85500f482b6447f8
Alexey Radul <axch@mit.edu>
2011-03-19 20:04:51 +0000

    The inliner should be graceful about single-form programs, and should name the thing it is operating on PROGRAM.

--- vl/fol/inline.scm
+++ vl/fol/inline.scm
@@ -11,25 +11,27 @@
 ;;; removal makes acyclic) of the static call graph.  See
 ;;; feedback-vertex-set.scm for discussion and implementation.
 
-(define (inline forms)
-  (let* ((procedure-body (inline-map forms)))
-    (define (inline? name)
-      (not (not (procedure-body name))))
-    (define (not-inline? form)
-      (or (not (definition? form))
-          (not (inline? (definiendum form)))))
-    (let walk ((program (filter not-inline? forms)))
-      ((on-subexpressions
-        (rule `((? name ,inline?) (?? args))
-              (->let `(,(walk (procedure-body name)) ,@args))))
-       program))))
+(define (inline program)
+  (if (begin-form? program)
+      (let* ((procedure-body (inline-map program)))
+        (define (inline? name)
+          (not (not (procedure-body name))))
+        (define (not-inline? form)
+          (or (not (definition? form))
+              (not (inline? (definiendum form)))))
+        (let walk ((program (filter not-inline? program)))
+          ((on-subexpressions
+            (rule `((? name ,inline?) (?? args))
+                  (->let `(,(walk (procedure-body name)) ,@args))))
+           program)))
+      program))
 
 ;;; The inline map maps all procedure names to either the lambda
-;;; expression that they are equivalent to if it was deicded that they
-;;; should be inlined, or #f if they should not.
+;;; expression that they are equivalent to if it was decided that they
+;;; should be inlined, or #f if it was decided that they should not.
 
-(define (inline-map forms)
-  (let* ((definitions (filter definition? forms))
+(define (inline-map program)
+  (let* ((definitions (filter definition? program))
          (defn-map (alist->eq-hash-table
                     (map (lambda (defn)
                            (cons (definiendum defn) defn))
