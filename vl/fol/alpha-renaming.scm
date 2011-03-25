e9abc391745b57543f1a63fa8c958d9278695326
Alexey Radul <axch@mit.edu>
2011-03-25 15:12:43 +0000

    Code order and some explanations.

--- vl/fol/alpha-renaming.scm
+++ vl/fol/alpha-renaming.scm
@@ -1,16 +1,28 @@
 (declare (usual-integrations))
 ;;;; Alpha renaming
 
-(define (extend-alpha-env env names)
-  (append
-   (map cons
-        names
-        (map (lambda (name)
-               (if (assq name env)
-                   (make-name name)
-                   name))
-             names))
-   env))
+;;; Renaming all variables so that nothing shadows anything else is
+;;; just a recursive descent on the structure of the code.  In fact,
+;;; it doesn't even need to be too picky about the differences between
+;;; various forms that do not bind names; it just has to catch the
+;;; ones that do.
+
+;;; The recursive traversal carries down an environment of which
+;;; symbols have been renamed to which others.  If a name being bound
+;;; doesn't shadow anything, it should be renamed to itself, otherwise
+;;; to a fresh name.  The traversal need not bring anything up besides
+;;; the renamed expression.
+
+(define (alpha-rename program)
+  ;; TODO Fix the bookkeeping of what names the primitives rely on
+  (define (needed-names primitive)
+    (list (primitive-name primitive)))
+  (alpha-rename-exp program
+   (map (lambda (name)
+          (cons name name))
+        (delete-duplicates
+         `(cons car cdr if define let vector vector-ref let-values values
+                ,@(append-map needed-names *primitives*))))))
 
 (define (alpha-rename-exp exp env)
   (cond ((assq exp env) => cdr)
@@ -37,16 +49,19 @@
                (alpha-rename-exp (cdr exp) env)))
         (else exp)))
 
-(define (alpha-rename program)
-  ;; TODO Fix the bookkeeping of what names the primitives rely on
-  (define (needed-names primitive)
-    (list (primitive-name primitive)))
-  (alpha-rename-exp program
-   (map (lambda (name)
-          (cons name name))
-        (delete-duplicates
-         `(cons car cdr if define let vector vector-ref let-values values
-                ,@(append-map needed-names *primitives*))))))
+(define (extend-alpha-env env names)
+  (append
+   (map cons
+        names
+        (map (lambda (name)
+               (if (assq name env)
+                   (make-name name)
+                   name))
+             names))
+   env))
+
+;;; With an analagous traversal, we can check whether two programs are
+;;; alpha-renamings of each other.
 
 (define (alpha-rename? exp1 exp2)
   (let loop ((exp1 exp1)
@@ -72,6 +87,10 @@
            (and (loop (car exp1) (car exp2) env)
                 (loop (cdr exp1) (cdr exp2) env)))
           (else (equal? exp1 exp2)))))
+
+;;; Also, we can check whether a program is already alpha renamed
+;;; (i.e. already contains no shadowing) by looking to see whether it
+;;; alpha renames to itself.
 
 (define (alpha-renamed? program)
   (equal? program (alpha-rename program)))
