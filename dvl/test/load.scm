31b5e81b0bd1e3900fff7e2d02788f2bbe3ec5a8
Alexey Radul <axch@mit.edu>
2011-04-06 09:00:46 +0100

    Line breaking.

--- dvl/test/load.scm
+++ dvl/test/load.scm
@@ -18,10 +18,15 @@
    (equal? #t (determined-answer '(let ((x (gensym))) (gensym= x x))))
    (equal? #f (determined-answer '(let ((x (gensym))) (gensym= x (gensym)))))
 
-   (equal? #t (union-free-answer
-	       '(let ((x (gensym))) (gensym= x (if (> (real 2) (real 1)) x (gensym))))))
-   (equal? #f (union-free-answer
-	       '(let ((x (gensym))) (gensym= x (if (< (real 2) (real 1)) x (gensym))))))
+   (equal? #t
+    (union-free-answer
+     '(let ((x (gensym)))
+        (gensym= x (if (> (real 2) (real 1)) x (gensym))))))
+
+   (equal? #f
+    (union-free-answer
+     '(let ((x (gensym)))
+        (gensym= x (if (< (real 2) (real 1)) x (gensym))))))
    )
 
  (for-each-example "../vl/examples.scm" define-union-free-example-test)
