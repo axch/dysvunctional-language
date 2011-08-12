282667405a316f8222401b0a13a06419a4474229
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-12 14:11:40 +0100

    Extract string from a name.

--- Language/Common.hs
+++ Language/Common.hs
@@ -2,6 +2,7 @@
 module FOL.Language.Common
     ( Real
     , Name (..)
+    , name2str
     , module Prelude
     )
     where
@@ -11,3 +12,6 @@ import Prelude hiding (Real)
 type Real = Double
 
 data Name = Name String deriving (Eq, Show)
+
+name2str :: Name -> String
+name2str (Name n) = n
