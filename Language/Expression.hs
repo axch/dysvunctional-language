387735a41cf8a72830ccde679de373331f0f0a65
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-12 10:09:52 +0100

    Make Expr parametrized over the type of names.

--- Language/Expression.hs
+++ Language/Expression.hs
@@ -4,55 +4,57 @@ module FOL.Language.Expression where
 import FOL.Language.Common
 import FOL.Language.Pretty
 
-data Prog = Prog [Defn] Expr deriving (Eq, Show)
+data Prog name = Prog [Defn name] (Expr name) deriving (Eq, Show)
 
-data Defn = Defn TypedVar [TypedVar] Expr deriving (Eq, Show)
+data Defn name = Defn (Typed name) [Typed name] (Expr name) deriving (Eq, Show)
 
-data Type = NilTy
-          | RealTy
-          | BoolTy
-          | ConsTy Type Type
-          | VectorTy [Type]
-          | ValuesTy [Type]
-            deriving (Eq, Show)
+data Type
+    = NilTy
+    | RealTy
+    | BoolTy
+    | ConsTy Type Type
+    | VectorTy [Type]
+    | ValuesTy [Type]
+      deriving (Eq, Show)
 
-data Expr = Var Name
-          -- Literals
-          | Nil
-          | Bool Bool
-          | Real Real
-          -- Special forms
-          | If Expr Expr Expr
-          | Let [LetBinding] Expr
-          | LetValues [LetValuesBinding] Expr
-          -- Access
-          | Car Expr
-          | Cdr Expr
-          | VectorRef Expr Integer
-          -- Construction
-          | Cons Expr Expr
-          | Vector [Expr]
-          | Values [Expr]
-          -- Procedure call
-          | ProcCall Name [Expr]
-            deriving (Eq, Show)
+data Expr name
+    = Var name
+    -- Literals
+    | Nil
+    | Bool Bool
+    | Real Real
+    -- Special forms
+    | If (Expr name) (Expr name) (Expr name)
+    | Let [LetBinding name] (Expr name)
+    | LetValues [LetValuesBinding name] (Expr name)
+    -- Access
+    | Car (Expr name)
+    | Cdr (Expr name)
+    | VectorRef (Expr name) Integer
+    -- Construction
+    | Cons (Expr name) (Expr name)
+    | Vector [Expr name]
+    | Values [Expr name]
+    -- Procedure call
+    | ProcCall name [Expr name]
+      deriving (Eq, Show)
 
-type TypedVar         = (Name, Type)
-type LetBinding       = (Name, Expr)
-type LetValuesBinding = ([Name], Expr)
+type Typed name            = (name,   Type     )
+type LetBinding name       = (name,   Expr name)
+type LetValuesBinding name = ([name], Expr name)
 
 -- Pretty-printing of programs
 
 instance Pretty Name where
     pp (Name n) = text n
 
-instance Pretty Prog where
+instance Pretty name => Pretty (Prog name) where
     pp (Prog []    expr) = pp expr
     pp (Prog defns expr) = parens $ text "begin" $+$ body
         where
           body = nest 1 (sepMap pp defns $+$ pp expr)
 
-instance Pretty Defn where
+instance Pretty name => Pretty (Defn name) where
     pp (Defn proc args body) = parens $ proto $+$ nest 1 (types $+$ pp body)
         where
           (proc_name, proc_type) = proc
@@ -68,7 +70,7 @@ instance Pretty Type where
     pp (VectorTy ts)  = ppForm "vector" ts
     pp (ValuesTy ts)  = ppForm "values" ts
 
-instance Pretty Expr where
+instance Pretty name => Pretty (Expr name) where
     pp Nil          = ppList []
     pp (Var x)      = pp x
     pp (Real r)     = real r
