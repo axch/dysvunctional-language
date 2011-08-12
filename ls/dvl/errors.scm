450b09754948743d84a8ca8bbf008d673e80af20
Alexey Radul <axch@mit.edu>
2011-08-12 11:55:23 +0100

    And now dvl errors are their own condition type.

--- ls/dvl/errors.scm
+++ ls/dvl/errors.scm
@@ -15,5 +15,26 @@
 
 (define syntax-error error)
 
+(define (report-dvl-error dvl-error port)
+  (format-error-message
+   (dvl-error-message dvl-error)
+   `(,(dvl-error-binding dvl-error)
+     ,@(dvl-error-irritants dvl-error))
+   port))
+
+(define condition-type:dvl-error
+  (make-condition-type
+   'dvl-error condition-type:simple-error '(binding) report-dvl-error))
+
+(define make-dvl-error
+  (condition-constructor condition-type:dvl-error '(message binding irritants)))
+(define dvl-error? (condition-predicate condition-type:dvl-error))
+(define dvl-error-message (condition-accessor condition-type:dvl-error 'message))
+(define dvl-error-binding (condition-accessor condition-type:dvl-error 'binding))
+(define dvl-error-irritants (condition-accessor condition-type:dvl-error 'irritants))
+(define signal-dvl-error
+  (condition-signaller
+   condition-type:dvl-error '(message binding irritants) standard-error-handler))
+
 (define (dvl-error message . irritants)
-  (apply error message *on-behalf-of* irritants))
+  (signal-dvl-error message *on-behalf-of* irritants))
