1d215693ff3924fc38ee42830f6b4c65b15cfad0
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-15 16:12:33 +0100

    The list of FOL primitives.

--- Language/Common.hs
+++ Language/Common.hs
@@ -2,6 +2,7 @@
 module FOL.Language.Common
     ( Real
     , Name (..)
+    , primitives
     , module Prelude
     )
     where
@@ -11,3 +12,13 @@ import Prelude hiding (Real)
 type Real = Double
 
 data Name = Name String deriving (Eq, Ord, Show)
+
+primitives :: [Name]
+primitives
+    = map Name [ "abs", "exp", "log"
+               , "sin", "cos", "tan"
+               , "asin", "acos", "sqrt", "real"
+               , "+" , "-", "*", "/", "atan", "expt"
+               , "zero?", "positive?", "negative?"
+               , "<", "<=", ">", ">=", "="
+               ]
