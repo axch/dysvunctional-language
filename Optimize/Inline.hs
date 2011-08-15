fef390a32690e806f7872c81183d709bff3c958d
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-15 16:12:45 +0100

    Cleanup and polish.

--- Optimize/Inline.hs
+++ Optimize/Inline.hs
@@ -15,78 +15,76 @@ import qualified Data.Map as Map
 inline :: Prog -> Unique Prog
 inline = alphaRn . inlineProg
 
-nonInlinableDefns :: [Defn] -> [Name]
-nonInlinableDefns = feedbackVertexSet . mkCallGraph
-
-mkCallGraph :: [Defn] -> [(Name, [Name])]
-mkCallGraph defns
-    = [ (proc_name, callees body `intersect` defined_procs)
-            | Defn (proc_name, _) _ body <- defns ]
-    where
-      defined_procs = [proc_name | Defn (proc_name, _) _ _ <- defns]
-
 callees :: Expr -> [Name]
 callees (Var x)  = []
 callees Nil      = []
 callees (Bool _) = []
 callees (Real _) = []
-callees (If p c a) = concat [ callees p , callees c, callees a]
+callees (If p c a)
+    = concat [callees p , callees c, callees a]
 callees (Let bindings body)
-    = concat $ (callees body) : [callees e | (_, e) <- bindings]
+    = concat $ callees body : [callees e | (_, e) <- bindings]
 callees (LetValues bindings body)
-    = concat $ (callees body) : [callees e | (_, e) <- bindings]
-callees (Car e) = callees e
-callees (Cdr e) = callees e
+    = concat $ callees body : [callees e | (_, e) <- bindings]
+callees (Car e)         = callees e
+callees (Cdr e)         = callees e
 callees (VectorRef e _) = callees e
-callees (Cons e1 e2) = callees e1 ++ callees e2
-callees (Vector es) = concatMap callees es
-callees (Values es) = concatMap callees es
-callees (ProcCall proc args) = proc : concatMap callees args
+callees (Cons e1 e2)    = callees e1 ++ callees e2
+callees (Vector es)     = concatMap callees es
+callees (Values es)     = concatMap callees es
+callees (ProcCall proc args)
+    = proc : concatMap callees args
 
 inlineProg :: Prog -> Prog
-inlineProg prog@(Prog defns expr) = Prog defns' expr'
+inlineProg (Prog defns expr) = Prog defns' expr'
     where
-      defined_procs = [proc_name | Defn (proc_name, _) _ _ <- defns]
-      non_inlinable = nonInlinableDefns defns
-      inlinable     = defined_procs \\ non_inlinable
-      inlinable_index
-          = Map.fromList [ (proc_name, defn)
-                               | defn@(Defn (proc_name, _) _ _) <- defns
-                               , proc_name `elem` inlinable ]
-      defns' = [ inlineDefn inlinable_index defn
-                     | defn@(Defn (proc_name, _) _ _) <- defns
-                     , proc_name `elem` non_inlinable ]
-      expr'  = inlineExpr inlinable_index expr
+      non_inlinable = feedbackVertexSet call_graph
+      call_graph    = [(proc_name, callees body \\ primitives)
+                           | Defn (proc_name, _) _ body <- defns]
+      defn_map      = Map.fromList
+                      [(proc_name, defn)
+                           | defn@(Defn (proc_name, _) _ _) <- defns
+                           , proc_name `notElem` non_inlinable]
+      defns'        = [inlineDefn defn_map defn
+                           | defn@(Defn (proc_name, _) _ _) <- defns
+                           , proc_name `elem` non_inlinable]
+      expr'         = inlineExpr defn_map expr
 
 inlineDefn :: Map Name Defn -> Defn -> Defn
-inlineDefn defns (Defn proc args body)
-    = Defn proc args (inlineExpr defns body)
+inlineDefn defn_map (Defn proc args body)
+    = Defn proc args (inlineExpr defn_map body)
 
 inlineExpr :: Map Name Defn -> Expr -> Expr
-inlineExpr _ e@(Var x)  = e
+inlineExpr _ e@(Var _)  = e
 inlineExpr _ e@Nil      = e
 inlineExpr _ e@(Bool _) = e
 inlineExpr _ e@(Real _) = e
-inlineExpr defns (If p c a) = If (inlineExpr defns p) (inlineExpr defns c) (inlineExpr defns a)
-inlineExpr defns (Let bindings body)
+inlineExpr defn_map (If p c a)
+    = If (inlineExpr defn_map p)
+         (inlineExpr defn_map c)
+         (inlineExpr defn_map a)
+inlineExpr defn_map (Let bindings body)
     = Let bindings' body'
     where
-      bindings' = [ (x, e') | (x, e) <- bindings, let e' = inlineExpr defns e ]
-      body' = inlineExpr defns body
-inlineExpr defns (LetValues bindings body)
+      bindings' = [ (x,  e') | (x,  e) <- bindings
+                             , let e' = inlineExpr defn_map e ]
+      body' = inlineExpr defn_map body
+inlineExpr defn_map (LetValues bindings body)
     = LetValues bindings' body'
     where
-      bindings' = [ (xs, e') | (xs, e) <- bindings, let e' = inlineExpr defns e ]
-      body' = inlineExpr defns body
-inlineExpr defns (Car e)         = Car (inlineExpr defns e)
-inlineExpr defns (Cdr e)         = Cdr (inlineExpr defns e)
-inlineExpr defns (VectorRef e i) = VectorRef (inlineExpr defns e) i
-inlineExpr defns (Cons e1 e2)    = Cons (inlineExpr defns e1) (inlineExpr defns e2)
-inlineExpr defns (Vector es)     = Vector (map (inlineExpr defns) es)
-inlineExpr defns (Values es)     = Values (map (inlineExpr defns) es)
-inlineExpr defns (ProcCall name args)
-    | Just (Defn proc formals body) <- Map.lookup name defns
-    , let bindings = zip (map fst formals) args
-    = inlineExpr defns $ Let bindings body
+      bindings' = [ (xs, e') | (xs, e) <- bindings
+                             , let e' = inlineExpr defn_map e ]
+      body' = inlineExpr defn_map body
+inlineExpr defn_map (Car e)         = Car (inlineExpr defn_map e)
+inlineExpr defn_map (Cdr e)         = Cdr (inlineExpr defn_map e)
+inlineExpr defn_map (VectorRef e i) = VectorRef (inlineExpr defn_map e) i
+inlineExpr defn_map (Cons e1 e2)
+    = Cons (inlineExpr defn_map e1) (inlineExpr defn_map e2)
+inlineExpr defn_map (Vector es)     = Vector (map (inlineExpr defn_map) es)
+inlineExpr defn_map (Values es)     = Values (map (inlineExpr defn_map) es)
+inlineExpr defn_map (ProcCall name args)
+    | Just (Defn _ vars body) <- Map.lookup name defn_map
+    , let bindings = zip (map fst vars) args
+    = inlineExpr defn_map $ Let bindings body
     | otherwise
-    = ProcCall name (map (inlineExpr defns) args)
+    = ProcCall name (map (inlineExpr defn_map) args)
