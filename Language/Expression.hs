b8a7b5f5d52ee65b6dfda9c33949b5a8785d46b3
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-18 17:48:21 +0100

    Print symbols in upper case.
    Following long standing Lisp tradition.  Also useful to visually
    distinguish symbols in error messages.

--- Language/Expression.hs
+++ Language/Expression.hs
@@ -4,6 +4,8 @@ module FOL.Language.Expression where
 import FOL.Language.Common
 import FOL.Language.Pretty
 
+import Data.Char
+
 data Prog = Prog [Defn] Expr deriving (Eq, Show)
 
 data Defn = Defn ShapedName [ShapedName] Expr deriving (Eq, Show)
@@ -43,14 +45,17 @@ type ShapedName       = (Name,   Shape)
 type LetBinding       = (Name,   Expr)
 type LetValuesBinding = ([Name], Expr)
 
+procName :: Defn -> Name
+procName (Defn (proc_name, _) _ _) = proc_name
+
 -- Pretty-printing of programs
 
 instance Pretty Name where
-    pp (Name n) = text n
+    pp (Name n) = text (map toUpper n)
 
 instance Pretty Prog where
     pp (Prog []    expr) = pp expr
-    pp (Prog defns expr) = parens $ text "begin" $+$ body
+    pp (Prog defns expr) = parens $ text "BEGIN" $+$ body
         where
           body = nest 1 (sepMap pp defns $+$ pp expr)
 
@@ -59,45 +64,45 @@ instance Pretty Defn where
         where
           (proc_name, proc_shape) = proc
           (arg_names, arg_shapes) = unzip args
-          proto  = text "define" <+> (parens $ pp proc_name <+> sepMap pp arg_names)
-          shapes = ppForm "argument-types" (arg_shapes ++ [proc_shape])
+          proto  = text "DEFINE" <+> (parens $ pp proc_name <+> sepMap pp arg_names)
+          shapes = ppForm "ARGUMENT-TYPES" (arg_shapes ++ [proc_shape])
 
 instance Pretty Shape where
     pp NilSh          = ppList []
-    pp RealSh         = text "real"
-    pp BoolSh         = text "bool"
-    pp (ConsSh t1 t2) = ppForm "cons"   [t1, t2]
-    pp (VectorSh ts)  = ppForm "vector" ts
-    pp (ValuesSh ts)  = ppForm "values" ts
+    pp RealSh         = text "REAL"
+    pp BoolSh         = text "BOOL"
+    pp (ConsSh t1 t2) = ppForm "CONS"   [t1, t2]
+    pp (VectorSh ts)  = ppForm "VECTOR" ts
+    pp (ValuesSh ts)  = ppForm "VALUES" ts
 
 instance Pretty Expr where
     pp Nil          = ppList []
     pp (Var x)      = pp x
     pp (Real r)     = real r
-    pp (Bool True)  = text "#t"
-    pp (Bool False) = text "#f"
+    pp (Bool True)  = text "#T"
+    pp (Bool False) = text "#F"
 
-    pp (If p c a) = parens $ text "if" <+> sepMap pp [p, c, a]
+    pp (If p c a) = parens $ text "IF" <+> sepMap pp [p, c, a]
 
     pp (Let bs e)
-        = parens $ (text "let" <+> bindings) $+$ (nest 1 body)
+        = parens $ (text "LET" <+> bindings) $+$ (nest 1 body)
         where
           body = pp e
           bindings = parens . sepMap ppBinding $ bs
           ppBinding (name, expr) = ppList [pp name, pp expr]
 
     pp (LetValues bs e)
-        = parens $ (text "let-values" <+> bindings) $+$ (nest 1 body)
+        = parens $ (text "LET-VALUES" <+> bindings) $+$ (nest 1 body)
         where
           body = pp e
           bindings = parens . sepMap ppBinding $ bs
           ppBinding (names, expr) = ppList [ppList (map pp names), pp expr]
 
-    pp (Car e)         = ppForm "car" [e]
-    pp (Cdr e)         = ppForm "cdr" [e]
-    pp (VectorRef e i) = ppList [text "vector-ref", pp e, int i]
-    pp (Cons e1 e2)    = ppForm "cons"   [e1, e2]
-    pp (Vector es)     = ppForm "vector" es
-    pp (Values es)     = ppForm "values" es
+    pp (Car e)         = ppForm "CAR" [e]
+    pp (Cdr e)         = ppForm "CDR" [e]
+    pp (VectorRef e i) = ppList [text "VECTOR-REF", pp e, int i]
+    pp (Cons e1 e2)    = ppForm "CONS"   [e1, e2]
+    pp (Vector es)     = ppForm "VECTOR" es
+    pp (Values es)     = ppForm "VALUES" es
 
     pp (ProcCall proc args) = ppList $ pp proc : map pp args
