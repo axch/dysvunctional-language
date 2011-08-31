9601e2a8dab21c2a2fbd42c72b92f1e9ae03350d
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-08-31 17:09:19 +0100

    Make the tidier flush unused bindings in LET and LET-VALUES.

--- Language/Tidy.hs
+++ Language/Tidy.hs
@@ -15,7 +15,7 @@ tidyExpr = rewrite rule
       rule e = msum (map ($ e) tidyRules)
 
 tidyRules :: [Expr -> Maybe Expr]
-tidyRules = [tidyLetValues]
+tidyRules = [tidyLetValues, flushUnusedBindings]
 
 tidyLetValues :: Expr -> Maybe Expr
 tidyLetValues e@(LetValues (Bindings bs) body)
@@ -29,6 +29,25 @@ tidyLetValues e@(LetValues (Bindings bs) body)
       is_values (_, Values _) = True
       is_values _             = False
 tidyLetValues _ = Nothing
+
+flushUnusedBindings :: Expr -> Maybe Expr
+flushUnusedBindings (Let (Bindings bs) body)
+    | bs == bs'
+    = Nothing
+    | otherwise
+    = Just $ mkLet bs' body
+    where
+      fvs = fv body
+      bs' = [(x, e) | (x, e) <- bs, x `elem` fvs]
+flushUnusedBindings (LetValues (Bindings bs) body)
+    | bs == bs'
+    = Nothing
+    | otherwise
+    = Just $ mkLetValues bs' body
+    where
+      fvs = fv body
+      bs' = [(xs, e) | (xs, e) <- bs, not (null (xs `intersect` fvs))]
+flushUnusedBindings _ = Nothing
 
 tidyDefn :: Defn -> Defn
 tidyDefn (Defn proc args body) = Defn proc args (tidyExpr body)
