8902d4ec894980f01479be88d859992d5c83a73b
Alexey Radul <axch@mit.edu>
2011-09-08 12:31:41 +0100

    Stage definitions might look like this.
    I still need to make the property requiring mechanism I envision work.

--- ls/fol/optimize.scm
+++ ls/fol/optimize.scm
@@ -22,6 +22,96 @@
 
 ;;; See doc/architecture.txt for discussion.
 
+#|
+;; Stage definitions might look like this
+
+;; The properties are
+;; syntax-checked
+;; type
+;; unique-names
+;; a-normal-form
+;; lets-lifted
+
+;; The possible clause types are
+;; requires
+;; preserves generates destroys
+;; computes
+;; idempotent
+
+;; TODO get property-generator to work
+;; TODO get the autowrappers to wrap the property generators
+
+(define-stage check-fol-types
+  check-program-types
+  (computes type)
+  (generates syntax-checked))
+
+(define-stage unique-rename
+  alpha-rename
+  (requires syntax-checked)
+  (generates unique-names)
+  (preserves a-normal-form lets-lifted type syntax-checked)
+  (idempotent))
+
+(define-stage inline
+  %inline
+  (requires syntax-checked)
+  (preserves syntax-checked type a-normal-form) ; lets-lifted?
+  (destroys unique-names)
+  (idempotent))                  ; not really, but on current examples
+
+(define-stage a-normal-form
+  approximate-anf
+  (requires syntax-checked)
+  (generates a-normal-form)
+  (preserves syntax-checked type unique-names)
+  (destroys lets-lifted)
+  (idempotent))
+
+(define-stage lift-lets
+  %lift-lets
+  ;; The latter just because I'm lazy
+  (requires syntax-checked a-normal-form)
+  (generates lets-lifted)
+  ;; TODO Does it really preserve a-normal-form ?
+  (preserves syntax-checked type unique-names a-normal-form))
+
+(define-stage scalar-replace-aggregates
+  %scalar-replace-aggregates
+  ;; TODO Does it require unique-names?
+  (requires syntax-checked a-normal-form)
+  (preserves syntax-checked type unique-names)
+  (destroys a-normal-form lets-lifted)) ; because of the reconstruction
+
+(define-stage intraprocedural-cse
+  %intraprocedural-cse
+  ;; The latter two requirements are not really requirements, but it
+  ;; works much better this way.
+  (requires syntax-checked unique-names a-normal-form lets-lifted)
+  (preserves syntax-checked type unique-names a-normal-form lets-lifted)
+  (idempotent))
+
+(define-stage eliminate-intraprocedural-dead-code
+  eliminate-intraprocedural-dead-variables
+  ;; TODO Does it really require unique names?
+  (requires syntax-checked unique-names)
+  (preserves syntax-checked type unique-names a-normal-form lets-lifted)
+  (idempotent))
+
+(define-stage eliminate-interprocedural-dead-code
+  ;; TODO Do I want to split this into the pure-interprocedural part
+  ;; and the intraprocedural part?  One problem: the former
+  ;; technically does not preserve type correctness, because it
+  ;; inserts tombstones which currently do not type properly.  In
+  ;; fact, this criticism can be levelled against the composition
+  ;; well, in general.
+  interprocedural-dead-code-elimination
+  ;; TODO Does it really require unique names?
+  (requires syntax-checked unique-names)
+  (preserves syntax-checked type unique-names a-normal-form lets-lifted)
+  (idempotent))
+|#
+
 (define (fol-optimize program)
   ((lambda (x) x) ; This makes the last stage show up in the stack sampler
    (reverse-anf
