4bc749fdeca23a7ba270d0740f881221c935de77
Alexey Radul <axch@mit.edu>
2011-09-20 16:28:32 +0100

    Rewrite the explanation of the inliner.
    Refactor the code to better agree with the explanation.  Notably, pull
    PERFORM-INLINING out of %%INLINE to name the process of actually
    inlining the procedures given, as opposed to orchestrating the whole
    show of computing the call graph, doing the graph crunch, and then
    inlining the result.

--- ls/fol/inline.scm
+++ ls/fol/inline.scm
@@ -1,15 +1,96 @@
 (declare (usual-integrations))
 ;;;; Inlining procedure definitions
 
-;;; Procedure definitions can be inlined arbitrarily, provided that
-;;; you don't try to inline all the edges of any cycle of the call
-;;; graph.  Since FOL is first order, the static call graph is a
-;;; decent approximation of the dynamic call graph, and the actual
-;;; inlining can be done by search and replace of call sites.  There
-;;; is one interesting step: to decide which procedures not to inline,
-;;; I need to compute a feedback vertex set (set of vertices whose
-;;; removal makes acyclic) of the static call graph.  See
-;;; feedback-vertex-set.scm for discussion and implementation.
+;;; Inlining procedure definitions produces the following benefits:
+;;; - Procedure boundaries are removed, giving later intraprocedural
+;;;   optimizations more purchase.
+;;; - Procedure bodies are cloned to their call sites, allowing
+;;;   per-call-site specializations and optimizations.
+;;; - Function call costs in the underlying implementation are
+;;;   removed.
+
+;;; Inlining procedure definitions also has the drawback of
+;;; (potentially) increasing code size, causing:
+;;; - Later stages to repeat work
+;;; - Worse instruction cache performance in the final output
+
+;;; This inliner makes the simplification that it will inline all or
+;;; none of the call sites of any given procedure.  Given that, the
+;;; question of selecting which procedures to inline can be reduced to
+;;; a choice of vertices of the static call graph.  Since FOL is first
+;;; order, the task of actually inlining the selected procedures
+;;; becomes a search-and-replace of their names by their bodies.
+
+(define (perform-inlining program names)
+  (let ((lookup-defn (definition-map program))
+        (walked-bodies (make-eq-hash-table)))
+    ;; Memoize performing the inlining on a particular procedure's
+    ;; body in an explicit hash table of promises to avoid repeating
+    ;; it for every call site.
+    (define (inline? name)
+      (not (not (walked-body name))))
+    (define (not-inline? form)
+      (or (not (definition? form))
+          (not (inline? (definiendum form)))))
+    (define (walk expression)
+      ((on-subexpressions
+        (rule `((? name ,inline?) (?? args))
+              (->let `(,(force (walked-body name)) ,@args))))
+       expression))
+    (define (walked-body name)
+      (hash-table/get walked-bodies name #f))
+    (for-each (lambda (name)
+                (hash-table/put!
+                 walked-bodies name
+                 (delay (walk (definiens (remove-defn-argument-types
+                                          (lookup-defn name)))))))
+              names)
+    (walk (filter not-inline? program))))
+
+;;; So, which vertices of the call graph should be inlined?  The
+;;; current strategy is to annotate the call graph with the
+;;; multiplicity of its edges and with the per-call-site code size
+;;; increase from inlining each procedure, and then greedily choose to
+;;; inline the procedures that give the smallest overall increase
+;;; until the size hits a threshold.
+
+;;; This annotated call graph is represented as a list of records of
+;;; the form
+;;;   (procedure-name inline-cost . callee-names)
+;;; where the list of callee names admits duplicates to indicate
+;;; multiplicity.  The procedure CALL-GRAPH computes this graph for a
+;;; given program.
+
+(define (call-graph program)
+  (define defined-name? (definition-map program))
+  (define (make-record id cost out-neighbors)
+    (cons id (cons cost out-neighbors)))
+  (define (defn-inline-cost defn)
+    (+ (count-pairs
+        (definiens (remove-defn-argument-types defn)))
+       ;; Let bindings, being a list of two-element lists, take a
+       ;; little more space than an apply with an explicit lambda,
+       ;; because that is two parallel lists.
+       (length (cdr (cadr defn)))))
+  (define (defn-vertex defn)
+    (make-record
+     (definiendum defn)
+     (defn-inline-cost defn)
+     (filter-tree defined-name? (definiens defn))))
+  (define entry-point-vertex
+    (let ((entry-point-name (make-name '%%main)))
+      (make-record
+       entry-point-name 0
+       (cons entry-point-name ; Entry point is not inlinable
+             (filter-tree defined-name? (entry-point program))))))
+  (cons
+   entry-point-vertex
+   (map defn-vertex (filter definition? program))))
+
+;;; The actual greedy graph algorithm is implemented by
+;;; ACCEPTABLE-INLINEES in inlinees.scm.  So the toplevel inliner just
+;;; picks a threshold and performs the inlining indicated by
+;;; ACCEPTABLE-INLINEES on the call graph of the program.
 
 (define (%inline program)
   (%%inline (count-pairs program) program))
@@ -17,39 +98,24 @@
 (define (%%inline size-increase-threshold program)
   (tidy-begin
    (if (begin-form? program)
-       (let ((procedure-bodies (inline-map size-increase-threshold program))
-             (walked-bodies (make-eq-hash-table)))
-         (define (inline? name)
-           (not (not (walked-body name))))
-         (define (not-inline? form)
-           (or (not (definition? form))
-               (not (inline? (definiendum form)))))
-         (define (walk expression)
-           ((on-subexpressions
-             (rule `((? name ,inline?) (?? args))
-                   (->let `(,(force (walked-body name)) ,@args))))
-            expression))
-         (define (walked-body name)
-           (hash-table/get walked-bodies name #f))
-         (for-each (lambda (datum)
-                     (hash-table/put! walked-bodies (car datum)
-                                      (delay (walk (cdr datum)))))
-                   procedure-bodies)
-         (walk (filter not-inline? program)))
+       (perform-inlining
+        program
+        (acceptable-inlinees
+         size-increase-threshold (call-graph program)))
        program)))
 
-;;; The inline map maps all procedure names to either the lambda
-;;; expression that they are equivalent to if it was decided that they
-;;; should be inlined, or #f if it was decided that they should not.
+;;; Historical note: in the previous world order, the set of vertices
+;;; not to inline was chosen as a feedback vertex set (set of vertices
+;;; whose removal makes acyclic) of the static call graph, and the
+;;; strategy was to inline everything else.  See
+;;; feedback-vertex-set.scm for discussion and implementation.  This
+;;; is mentioned because the feedback vertex set concept is a useful
+;;; one to remember, and the code is still around.  Note that the
+;;; implementation of the threshold approach contains a clause
+;;; preventing the inlining of procedures that call themselves.
 
-(define (inline-map size-increase-threshold program)
-  (let* ((lookup (definition-map program))
-         (call-graph (call-graph program))
-         (inlinee-names (acceptable-inlinees size-increase-threshold call-graph)))
-    (map cons inlinee-names
-         (map definiens
-              (map remove-defn-argument-types
-                   (map lookup inlinee-names))))))
+;;; The definition map is a facility for looking up the full
+;;; definition form for a FOL procedure given its name.
 
 (define (definition-map program)
   (define defn-map
@@ -60,26 +126,3 @@
   (lambda (name)
     (hash-table/get defn-map name #f)))
 
-(define (call-graph program)
-  (define (defn-inline-cost defn)
-    (+ (count-pairs
-        (definiens (remove-defn-argument-types defn)))
-       ;; Let bindings, being a list of two-element lists, take a
-       ;; little more space than an apply with an explicit lambda,
-       ;; because that is two parallel lists.
-       (length (cdr (cadr defn)))))
-  (define (make-record id cost out-neighbors)
-    (cons id (cons cost out-neighbors)))
-  (let ((defined-name? (definition-map program)))
-    (cons
-     (let ((entry-point-name (make-name '%%main)))
-       (make-record
-        entry-point-name 0
-        (cons entry-point-name ; Entry point is not inlinable
-              (filter-tree defined-name? (entry-point program)))))
-     (map (lambda (defn)
-            (make-record
-             (definiendum defn)
-             (defn-inline-cost defn)
-             (filter-tree defined-name? (definiens defn))))
-          (filter definition? program)))))
