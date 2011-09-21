a40a467513c8890a3886155c46ca98956d252332
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-09-21 17:00:12 +0100

    Augment the compiler to decorate expressions with their types using `the' form.
    Unfortunately, this doesn't always work because the collected types
    are not always correct: expressions that are symbols and occur in
    different places in the source code will have the same type.
    
    Should I write this compiler in Haskell?  My type-checker at least
    doesn't have this problem (and doesn't require hacking).

--- ls/fol/common-lisp.scm
+++ ls/fol/common-lisp.scm
@@ -2,24 +2,29 @@
 ;;;; Simplistic FOL to Common Lisp compiler.
 
 (define (compile-program program)
-  (define compile-definition
-    (rule `(define ((? name ,fol-var?) (?? formals))
-             (argument-types (?? formal-types) (? return-type))
-             (? body))
-          `(defun ,name (,@formals)
-             (declare ,@(map (lambda (formal-type formal)
-                               `(type ,(fol-shape->type-specifier formal-type)
-                                      ,formal))
-                             formal-types
-                             formals))
-             ,(compile-expression body))))
-  (if (begin-form? program)
-      `(progn
-        ,@prelude
-        ,@(map compile-definition
-               (cdr (except-last-pair program)))
-        ,(compile-expression (last program)))
-      (compile-expression program)))
+  (let ((inferred-type-map (make-eq-hash-table)))
+    (check-program-types program inferred-type-map)
+    (define (lookup-inferred-type expr)
+      (or (hash-table/get inferred-type-map expr #f)
+          (error "Looking up unknow expression" expr)))
+    (define compile-definition
+      (rule `(define ((? name ,fol-var?) (?? formals))
+               (argument-types (?? formal-types) (? return-type))
+               (? body))
+            `(defun ,name (,@formals)
+               (declare ,@(map (lambda (formal-type formal)
+                                 `(type ,(fol-shape->type-specifier formal-type)
+                                        ,formal))
+                               formal-types
+                               formals))
+               ,(compile-expression body lookup-inferred-type))))
+    (if (begin-form? program)
+        `(progn
+          ,@prelude
+          ,@(map compile-definition
+                 (cdr (except-last-pair program)))
+          ,(compile-expression (last program) lookup-inferred-type))
+        (compile-expression program lookup-inferred-type))))
 
 (define (fol-shape->type-specifier shape)
   (cond ((eq? 'real shape)
@@ -64,69 +69,67 @@
       (declare (type symbol gensym1 gensym2))
       (eq gensym1 gensym2))))
 
-(define (compile-expression expr)
-  (cond ((fol-var? expr)
-         (compile-var expr))
-        ((fol-const? expr)
-         (compile-const expr))
-        ((if-form? expr)
-         (compile-if expr))
-        ((let-form? expr)
-         (compile-let expr))
-        ((let-values-form? expr)
-         (compile-let-values expr))
-        ((lambda-form? expr)
-         (compile-lambda expr))
-        ((vector-ref-form? expr)
-         (compile-vector-ref expr))
-        (else
-         (compile-application expr))))
-
-(define (compile-var expr)
-  (if (eq? 'real expr) 'identity expr))
-
-(define (compile-const expr)
-  (cond ((number? expr)
-         `(coerce ,expr 'double-float))
-        ((boolean? expr)
-         (if expr 't 'nil))
-        (else
-         'nil)))
-
-(define compile-if
-  (rule `(if (? pred)
-             (? cons)
-             (? alt))
-        `(if ,(compile-expression pred)
-             ,(compile-expression cons)
-             ,(compile-expression alt))))
-
-(define compile-let
-  (rule `(let ((?? bindings))
-           (? body))
-        `(let (,@(map (lambda (binding)
-                        `(,(car binding) ,(compile-expression (cadr binding))))
-                      bindings))
-           ,(compile-expression body))))
-
-(define compile-let-values
-  (rule `(let-values ((?? names) (? expr))
-           (? body))
-        `(muliple-values-bind (,@names) ,(compile-expression expr)
-           ,(compile-expression body))))
-
-(define compile-lambda
-  (rule `(lambda ((? var))
-           (? body))
-        `(function
-          (lambda (,var)
-            ,(compile-expression body)))))
-
-(define compile-vector-ref
-  (rule `(vector-ref (? expr) (? index))
-        `(svref ,(compile-expression expr) ,index)))
-
-(define vector-ref-form? (tagged-list? 'vector-ref))
-
-(define (compile-application expr)
-  (map compile-expression expr))
+(define (compile-expression expr lookup-inferred-type)
+  (define (%compile-expression expr)
+    `(the ,(fol-shape->type-specifier (lookup-inferred-type expr))
+          ,(%%compile-expression expr)))
+  (define (%%compile-expression expr)
+    (cond ((fol-var? expr) expr)
+          ((fol-const? expr)
+           (compile-const expr))
+          ((if-form? expr)
+           (compile-if expr))
+          ((let-form? expr)
+           (compile-let expr))
+          ((let-values-form? expr)
+           (compile-let-values expr))
+          ((lambda-form? expr)
+           (compile-lambda expr))
+          ((vector-ref-form? expr)
+           (compile-vector-ref expr))
+          (else
+           (compile-application expr))))
+  (define (compile-const expr)
+    (cond ((number? expr)
+           `(coerce ,expr 'double-float))
+          ((boolean? expr)
+           (if expr 't 'nil))
+          (else
+           'nil)))
+  (define compile-if
+    (rule `(if (? pred)
+               (? cons)
+               (? alt))
+          `(if ,(%compile-expression pred)
+               ,(%compile-expression cons)
+               ,(%compile-expression alt))))
+  (define compile-let
+    (rule `(let ((?? bindings))
+             (? body))
+          `(let (,@(map (lambda (binding)
+                          `(,(car binding)
+                            ,(%compile-expression (cadr binding))))
+                        bindings))
+             ,(%compile-expression body))))
+  (define compile-let-values
+    (rule `(let-values ((?? names) (? expr))
+             (? body))
+          `(muliple-values-bind (,@names)
+                                ,(%compile-expression expr)
+                                ,(%compile-expression body))))
+  (define compile-lambda
+    (rule `(lambda ((? var))
+             (? body))
+          `(function
+            (lambda (,var)
+              ,(%compile-expression body)))))
+  (define compile-vector-ref
+    (rule `(vector-ref (? expr) (? index))
+          `(svref ,(%compile-expression expr) ,index)))
+  (define vector-ref-form? (tagged-list? 'vector-ref))
+  (define (compile-application expr)
+    `(,(if  (eq? 'real (car expr))
+            'identity
+            (car expr))
+      ,@(map %compile-expression (cdr expr))))
+  (%compile-expression expr))
