8b6d60b5bf9874fbbd61e814d68ebd778f1c88d2
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-09-21 14:02:07 +0100

    Add type declarations to function definitions.
    Transform each FOL shape into a CL type specifier.  There is an almost
    perfect match, except that CL doesn't support heterogeneous vector
    types (which doesn't mean vectors can't be heterogeneous).  In order
    to make CL recognize literate constants as DOUBLE-FLOATs, they need to
    be formatted using a special syntax (like 3.0d0, which MIT Scheme
    understands, but doesn't emit, whereas CL treats 3. as an integer 3).
    I don't know how to do this from Scheme (modify the pretty-printer?),
    so for the time being I am simply going to wrap each constant X into
    (COERCE 'DOUBLE-FLOAT X).  Also, to avoid problems with REAL, it is
    replaced with IDENTITY.
    
    Example programs that the compiler produces compile with SBCL's
    default optimization settings, and fail to compile if I raise speed
    level to 3, because SBCL cannot infer resolve some ambiguous type.  A
    way to resolve this problem is to wrap every expression (whose type we
    know) into THE form.

--- ls/fol/common-lisp.scm
+++ ls/fol/common-lisp.scm
@@ -4,9 +4,14 @@
 (define (compile-program program)
   (define compile-definition
     (rule `(define ((? name ,fol-var?) (?? formals))
-             (argument-types (?? stuff))
+             (argument-types (?? formal-types) (? return-type))
              (? body))
           `(defun ,name (,@formals)
+             (declare ,@(map (lambda (formal-type formal)
+                               `(type ,(fol-shape->type-specifier formal-type)
+                                      ,formal))
+                             formal-types
+                             formals))
              ,(compile-expression body))))
   (if (begin-form? program)
       `(progn
@@ -16,32 +21,52 @@
         ,(compile-expression (last program)))
       (compile-expression program)))
 
+(define (fol-shape->type-specifier shape)
+  (cond ((eq? 'real shape)
+         'double-float)
+        ((eq? 'bool shape)
+         'boolean)
+        ((null? shape)
+         'null)
+        ((eq? 'cons (car shape))
+         `(cons ,(fol-shape->type-specifier (cadr  shape))
+                ,(fol-shape->type-specifier (caddr shape))))
+        ;; Heterogenious vector types are not supported by CL.
+        ((eq? 'vector (car shape))
+         `(simple-vector ,(length (cdr shape))))
+        ((eq? 'values (car shape))
+         `(values ,@(map fol-shape->type-specifier (cdr shape))))
+        (else
+         (error "Bogus shape " shape))))
+
 (define prelude
   '((declaim (inline zero?
                      positive?
                      negative?
                      read-real
                      write-real
-                     real               ; TODO REAL is a locked name
                      gensym=))
     (defun zero? (x)
+      (declare (type double-float x))
       (zerop x))
     (defun positive? (x)
+      (declare (type double-float x))
       (plusp x))
     (defun negative? (x)
+      (declare (type double-float x))
       (minusp x))
     (defun read-real ()
       (read))
     (defun write-real (x)
+      (declare (type double-float x))
       (format t "~F~%" x))
-    (defun real (x)
-      x)
     (defun gensym= (gensym1 gensym2)
+      (declare (type symbol gensym1 gensym2))
       (eq gensym1 gensym2))))
 
 (define (compile-expression expr)
   (cond ((fol-var? expr)
-         expr)
+         (compile-var expr))
         ((fol-const? expr)
          (compile-const expr))
         ((if-form? expr)
@@ -57,9 +82,12 @@
         (else
          (compile-application expr))))
 
+(define (compile-var expr)
+  (if (eq? 'real expr) 'identity expr))
+
 (define (compile-const expr)
   (cond ((number? expr)
-         expr)
+         `(coerce ,expr 'double-float))
         ((boolean? expr)
          (if expr 't 'nil))
         (else
