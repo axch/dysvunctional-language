fd632394bc45470289124b2f8bba72868f6ddc77
Alexey Radul <axch@mit.edu>
2011-10-12 18:30:21 +0100

    Draft a driver program for running the compiled DVL from SBCL.

---
+++ ls/dvl/examples/celestial-driver.lisp
@@ -0,0 +1,23 @@
+(defmacro delay (exp)
+  `(lambda () ,exp))
+
+(defun force (thunk)
+  (funcall thunk))
+
+(defun constant-arg-for-dvl-stream (argument dvl-stream)
+  (labels ((do-it (dvl-stream)
+             (cons (car dvl-stream)
+                   (delay (do-it (funcall (cdr dvl-stream) argument))))))
+    (do-it dvl-stream)))
+
+(defun stream-for-each (f stream)
+  (cons (funcall f (car stream))
+        (delay (stream-for-each f (force (cdr stream))))))
+
+(defun stream-take (count stream)
+  (if (= count 0)
+      stream
+      (stream-take (- count 1) (force (cdr stream)))))
+
+(defun drive ()
+ (stream-take 2 (stream-for-each #'pprint (constant-arg-for-dvl-stream 10.0d0 (__main__)))))
