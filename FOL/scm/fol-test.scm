50509bc961e411a62f6b4e9303ef975f5ee8409b
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-12-25 16:57:02 +0000

    Fix the test suite.
    More specifically:
    - remove the 'fol' target from Rakefile (we use Cabal to build the
      optimizer now);
    - Change the executable to 'fol2fol' and unregister the last test.  It
      doesn't pass because the lexical analyzer doesn't recognize Scheme
      identifiers of the form |foo|, and I am lazy to fix this now.
    - Fix the paths in the load script.
    Running `rake` in the root directory should run the test suite, which
    should pass provided that fol2fol has been built.

--- FOL/scm/fol-test.scm
+++ FOL/scm/fol-test.scm
@@ -5,21 +5,21 @@
 (define (define-external-fol-test dvl-program)
   (define-test
     (let* ((fol-program                 (compile-to-raw-fol dvl-program))
-           (optimized-fol-program       (external-fol-optimize fol-program))
-           (fol-program-value           (fol-eval fol-program))
-           (optimized-fol-program-value (fol-eval optimized-fol-program)))
+	   (optimized-fol-program       (external-fol-optimize fol-program))
+	   (fol-program-value           (fol-eval fol-program))
+	   (optimized-fol-program-value (fol-eval optimized-fol-program)))
       (check (equal? fol-program-value optimized-fol-program-value)))))
 
-(define *fol-executable* "/home/manzyuk/Projects/FOL/fol")
+(define *fol-executable* "fol2fol")
 
 (define (pipe-through command input)
   (with-output-to-string
    (lambda ()
      (with-input-from-string input
        (lambda ()
-         (run-shell-command command
-                            'input  (current-input-port)
-                            'output (current-output-port)))))))
+	 (run-shell-command command
+			    'input  (current-input-port)
+			    'output (current-output-port)))))))
 
 (define (write-to-string object)
   (with-output-to-string (lambda () (write object))))
@@ -37,75 +37,76 @@
 
 (define-external-fol-test
   '(let ((addn (lambda (n)
-                 (lambda (x) (+ n x)))))
+		 (lambda (x) (+ n x)))))
      (let ((add5 (addn (real 5.))))
        (add5 (real 3.)))))
 
 (define-external-fol-test
   '(let ((double (lambda (x) (+ x x)))
-         (square (lambda (x) (* x x)))
-         (compose (lambda (f g)
-                    (lambda (x) (f (g x))))))
+	 (square (lambda (x) (* x x)))
+	 (compose (lambda (f g)
+		    (lambda (x) (f (g x))))))
      (cons ((compose double square) (real 2.))
-           ((compose square double) (real 2.)))))
+	   ((compose square double) (real 2.)))))
 
 (define-external-fol-test
   '(let ((cube (lambda (x) (* x (* x x)))))
      (let ((enlarge-upto (lambda (bound)
-                           (lambda (x)
-                             (if (< x bound)
-                                 (cube x)
-                                 x)))))
+			   (lambda (x)
+			     (if (< x bound)
+				 (cube x)
+				 x)))))
        ((enlarge-upto (real 20.)) (real 3.)))))
 
 (define-external-fol-test
   '(let ()
      (define (fact n)
        (if (= n 0)
-           1
-           (* n (fact (- n 1)))))
+	   1
+	   (* n (fact (- n 1)))))
      (fact (real 4.))))
 
 (define-external-fol-test
   '(let ((increment (lambda (x) (+ x 1)))
-         (double (lambda (x) (* x 2)))
-         (car (lambda ((cons x ())) x))
-         (cdr (lambda ((cons () y)) y)))
+	 (double (lambda (x) (* x 2)))
+	 (car (lambda ((cons x ())) x))
+	 (cdr (lambda ((cons () y)) y)))
      (letrec ((map (lambda (f lst)
-                     (if (null? lst)
-                         ()
-                         (cons (f (car lst)) (map f (cdr lst)))))))
+		     (if (null? lst)
+			 ()
+			 (cons (f (car lst)) (map f (cdr lst)))))))
        (cons (map increment (real 1.) (real 2.) (real 3.) ())
-             (map double (real 4.) (real 5.) ())))))
+	     (map double (real 4.) (real 5.) ())))))
 
 (define-external-fol-test
   '(let ()
      (define (heron-step x)
        (lambda (guess)
-         (/ (+ guess (/ x guess)) 2)))
+	 (/ (+ guess (/ x guess)) 2)))
 
      (define (close-enuf? a b)
        (< (abs (- a b)) 0.00001))
 
      (define (numeric-fix f start close-enuf?)
        (let loop ((old start)
-                  (new (f start)))
-         (if (close-enuf? old new)
-             new
-             (loop new (f new)))))
+		  (new (f start)))
+	 (if (close-enuf? old new)
+	     new
+	     (loop new (f new)))))
 
      (define (square-root x)
        (numeric-fix (heron-step x) (real 1.0) close-enuf?))
 
      (cons (sqrt 2.) (square-root (real 2.)))))
 
+#;
 (define-external-fol-test
   '(letrec ((even? (lambda (n)
-                     (if (= n 0)
-                         #t
-                         (odd? (- n 1)))))
-            (odd? (lambda (n)
-                    (if (= n 0)
-                        #f
-                        (even? (- n 1))))))
+		     (if (= n 0)
+			 #t
+			 (odd? (- n 1)))))
+	    (odd? (lambda (n)
+		    (if (= n 0)
+			#f
+			(even? (- n 1))))))
      (even? (real 5.))))
--- FOL/scm/load.scm
+++ FOL/scm/load.scm
@@ -1,3 +1,13 @@
-(load "/home/manzyuk/Projects/BCL-AD/ls/dvl/load")
-(load "/home/manzyuk/Projects/BCL-AD/ls/testing/load")
+(define (self-relatively thunk)
+  (if (current-eval-unit #f)
+      (with-working-directory-pathname
+       (directory-namestring (current-load-pathname))
+       thunk)
+      (thunk)))
+
+(define (load-relative filename)
+  (self-relatively (lambda () (load filename))))
+
+(load-relative "../../ls/dvl/load")
+(load-relative "../../ls/testing/load")
 (load-relative-compiled "fol-test")
