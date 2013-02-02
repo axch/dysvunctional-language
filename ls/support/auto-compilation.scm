10aab541e773a942b316e7695427c89eff6925e2
Alexey Radul <axch@mit.edu>
2013-02-02 14:02:56 -0500

    Explanations and complete auto-compilation facilities.

--- ls/support/auto-compilation.scm
+++ ls/support/auto-compilation.scm
@@ -1,4 +1,23 @@
-;;; Only works on MIT Scheme
+(declare (usual-integrations))
+
+;;;; Auto-compilation
+
+;;; A facility for automatically (re)compiling files at load time so
+;;; as to avoid both the hassle of manual recompilations and the
+;;; slowness of running interpreted code.  Takes care around macros
+;;; from previously loaded files.
+
+(define (self-relatively thunk)
+  (let ((place (ignore-errors current-load-pathname)))
+    (if (pathname? place)
+	(with-working-directory-pathname
+	 (directory-namestring place)
+	 thunk)
+	(thunk))))
+
+(define (load-relative filename #!optional environment)
+  (self-relatively (lambda () (load filename environment))))
+
 (define (compiled-code-type)
   ;; Trying to support the C backend
   (if (lexical-unbound?
@@ -7,6 +26,8 @@
       "com"
       (compiler:compiled-code-pathname-type)))
 
+;; The environment argument is the one to take macro definitions from
+;; for sf.
 (define (cf-conditionally filename #!optional environment)
   (define (default-environment)
     (if (current-eval-unit #f)
