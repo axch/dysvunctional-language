c1f134d154cb3ceb0ea74ed6c45e785bb3c4ec35
Alexey Radul <axch@mit.edu>
2013-03-30 16:15:41 -0400

    Start testing effectiveness in the presence of user types.

--- ls/fol/test/backend-test.scm
+++ ls/fol/test/backend-test.scm
@@ -11,6 +11,15 @@
             (* n (fact (- n 1)))))
       (fact 4)))
 
+ (define magnitude
+   '(begin
+      (define-type point (structure (x real) (y real)))
+      (define (magnitude v)
+        (argument-types point real)
+        (sqrt (+ (* (point-x v) (point-x v))
+                 (* (point-y v) (point-y v)))))
+      (magnitude (make-point 1 1))))
+
  (define-each-check
    (equal? '((labels
               ((fact (n)
@@ -21,5 +30,8 @@
                     (* n (fact (- n (coerce 1. 'double-float))))))
                (__main__ () (fact (coerce 4. 'double-float))))
               (setf (fdefinition '__main__) (function __main__))))
-           (prepare-for-common-lisp factorial)))
+           (prepare-for-common-lisp factorial))
+
+   (equal? '()
+           (prepare-for-common-lisp magnitude)))
  )
