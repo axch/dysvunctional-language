152b8e68075a1272f763c608523dcb7cb8e7c375
Alexey Radul <axch@mit.edu>
2013-06-23 12:17:10 -0400

    Draft a grammatical structure for the command line.

--- ls/fol/entry-point.scm
+++ ls/fol/entry-point.scm
@@ -1,5 +1,29 @@
 ;; The commandline script lands here
 
+;; optimize n times; compile [via backend] are independent; execute
+;; implies compile, but admits "interpreted fol" backend, for which
+;; compiling is a no-op.
+
+;; the mit scheme backend has a standalone option; Stalin, SBCL, and
+;; Haskell are all standalone.
+
+;; How to link to the compiled output afterwards?
+
+;; fol [verb] file [via backend] [optimizing never|once|twice|thrice|n times|until convergence] [dumping <big-stage>] [adverb]*
 (define (fol-main arg)
-  (display arg)
-  (%exit 0))
+  (define (find-verb args)
+    (cond ((null? args)
+           (error "Need non-empty argument list"))
+          ((memq (car args) '(optimize compile run))
+           (car args))
+          (else #f)))
+  (define (parse-modifiers args)
+    '())
+  (let* ((args (with-input-from-string (string-append "(" arg ")") read))
+         (verb (find-verb args))
+         (args (if verb (cdr args) args))
+         (file (symbol->string (car args)))
+         (args (cdr args))
+         (modifiers (parse-modifiers args)))
+    (pp `(,verb ,file ,modifiers))
+    (%exit 0)))
