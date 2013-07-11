2509181589a0b073df581fa1795c4a8ba22e5aed
Alexey Radul <axch@mit.edu>
2013-07-11 17:50:36 -0400

    Copyright headers.

--- support/load.scm
+++ support/load.scm
@@ -1,3 +1,22 @@
+;;; ----------------------------------------------------------------------
+;;; Copyright 2010-2013 Alexey Radul.
+;;; ----------------------------------------------------------------------
+;;; This file is part of DysVunctional Language.
+;;; 
+;;; DysVunctional Language is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU Affero General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;;  License, or (at your option) any later version.
+;;; 
+;;; DysVunctional Language is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;; 
+;;; You should have received a copy of the GNU Affero General Public License
+;;; along with DysVunctional Language.  If not, see <http://www.gnu.org/licenses/>.
+;;; ----------------------------------------------------------------------
+
 (define (self-relatively thunk)
   (if (current-eval-unit #f)
       (with-working-directory-pathname
