2a1d9fe742c514da4685d4b4bced53a659912f1e
Alexey Radul <axch@mit.edu>
2013-08-08 13:43:08 -0400

    Now this code is presentable.

--- dvl/doc/boston-slug-talk-aug-2013/mandel.js
+++ dvl/doc/boston-slug-talk-aug-2013/mandel.js
@@ -2,7 +2,7 @@ function fol_program(stdlib, foreign, heap) {
   "use asm";
   var heap_view = new stdlib.Float32Array(heap);
   var sqrt = stdlib.Math.sqrt;
-  function operation_231(count, c_x, c_y, z_x, z_y) {
+  function op_231(count, c_x, c_y, z_x, z_y) {
     count = +count;
     c_x = +c_x;
     c_y = +c_y;
@@ -13,17 +13,18 @@ function fol_program(stdlib, foreign, heap) {
       heap_view[1] = z_y;
       return;
     } else {
-      return operation_231(count-1.0, c_x, c_y,
-                           ((z_x*z_x - z_y*z_y) + c_x),
-                           ((z_x*z_y + z_y*z_x) + c_y));
+      return op_231(count - 1.0, c_x, c_y,
+                    ((z_x*z_x - z_y*z_y) + c_x),
+                    ((z_x*z_y + z_y*z_x) + c_y));
     }
   }
+
   function __main__(x, y) {
     x = +x;
     y = +y;
     var z_x = 0.0;
     var z_y = 0.0;
-    operation_231(400.0, x, y, 0.0, 0.0);
+    op_231(400.0, x, y, 0.0, 0.0);
     z_x = +heap_view[0];
     z_y = +heap_view[1];
     return (sqrt(z_x*z_x + z_y*z_y) < 2.0)|0;
