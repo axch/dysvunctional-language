561c8dd0e87eb393f230f7cc2eb41736859fa2db
Alexey Radul <axch@mit.edu>
2013-08-07 22:31:20 -0400

    Separate computing the answer into a function of its own with controllable grid size.
    The goal is to later be able to do progressive improvement of the
    resolution of the image.

--- dvl/examples/mandel-driver.js
+++ dvl/examples/mandel-driver.js
@@ -27,17 +27,15 @@ function setPixel(imageData, x, y, r, g, b, a) {
     imageData.data[index+3] = a;
 }
 
-function doit() {
-    canvas = document.getElementById("it");
-    context = canvas.getContext("2d");
-    width = canvas.width;
-    height = canvas.height;
-    imageData = context.createImageData(width, height);
+function dataFor(width, height) {
     console.time("compute loop");
     var aliasDepth = 2;
+    var answer = new Array(width);
     for (var i = 0; i < width; i++) {
         var x = 3*(+i)/width - 2.0;
         var dx = 1.0/(aliasDepth*width);
+        var subAnswer = new Array(height);
+        answer[i] = subAnswer;
         for (var j = 0; j < height; j++) {
             var y = 3*(+j)/height - 1.5;
             var dy = 1.0/(aliasDepth*height);
@@ -48,10 +46,26 @@ function doit() {
                 }
             }
             color = 240-(240*ct/(aliasDepth*aliasDepth));
-            setPixel(imageData, i, j, color, color, color, 255);
+            answer[i][j] = color;
         }
     }
     console.timeEnd("compute loop");
+    return answer;
+}
+
+function doit() {
+    canvas = document.getElementById("it");
+    context = canvas.getContext("2d");
+    width = canvas.width;
+    height = canvas.height;
+    imageData = context.createImageData(width, height);
+    colorData = dataFor(width, height);
+    for (var i = 0; i < width; i++) {
+        for (var j = 0; j < height; j++) {
+            color = colorData[i][j];
+            setPixel(imageData, i, j, color, color, color, 255);
+        }
+    }
     context.putImageData(imageData, 0, 0);
 }
             
