3d74449053a6a96bc31400db64821f78a33b21e7
Alexey Radul <axch@mit.edu>
2013-08-07 22:11:00 -0400

    Enlarge and antialias the picture, and time the computation (also make the background gray instead of blue).

--- dvl/examples/mandel-driver.js
+++ dvl/examples/mandel-driver.js
@@ -33,15 +33,25 @@ function doit() {
     width = canvas.width;
     height = canvas.height;
     imageData = context.createImageData(width, height);
+    console.time("compute loop");
+    var aliasDepth = 2;
     for (var i = 0; i < width; i++) {
+        var x = 3*(+i)/width - 2.0;
+        var dx = 1.0/(aliasDepth*width);
         for (var j = 0; j < height; j++) {
-            if(mandel(3*(+i)/width - 1.5, 3*(+j)/height - 1.5)) {
-                setPixel(imageData, i, j, 0, 0, 0, 255);
-            } else {
-                setPixel(imageData, i, j, 0, 0, 255, 255);
+            var y = 3*(+j)/height - 1.5;
+            var dy = 1.0/(aliasDepth*height);
+            var ct = 0;
+            for (var ii = 0; ii < aliasDepth; ii++) {
+                for (var jj = 0; jj < aliasDepth; jj++) {
+                    if(mandel(x+ii*dx, y+jj*dy)) {ct = ct + 1;}
+                }
             }
+            color = 240-(240*ct/(aliasDepth*aliasDepth));
+            setPixel(imageData, i, j, color, color, color, 255);
         }
     }
+    console.timeEnd("compute loop");
     context.putImageData(imageData, 0, 0);
 }
             
